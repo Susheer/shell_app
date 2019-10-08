080a7495084a318a5c96b5f034218e60cab71f63
Sudheer <sudheergupta0508@gmail.com>
2019-10-08 13:21:55 +0530

    flag ctl added with img

--- server/model/Match.js
+++ server/model/Match.js
@@ -4,13 +4,17 @@ let Schema = mongoose.Schema;
 let matchesSchema = new Schema({
   title: { type: String, required: true },
   sportsName: { type: Number, required: true },
-  status: { type: String },
+  status: { type: String, default: "" },
+  status_color: { type: String, default: "" },
   startDate: { type: Date, default: Date.now },
   flag_a: { type: String, required: true },
   flag_b: { type: String, required: true },
   team_a: { type: String, required: true },
   team_b: { type: String, required: true },
   enable: { type: Boolean, required: true, default: true },
+
+  flag_a_color: { type: String },
+  flag_b_color: { type: String },
   createdDate: { type: Date, default: Date.now },
   updatedDate: { type: Date, default: Date.now }
 });
